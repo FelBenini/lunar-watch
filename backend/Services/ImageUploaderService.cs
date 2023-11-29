49f615a245c4dc9ae13480adebbc9ac72a3aa5b6
FelBenini <felbenini@gmail.com>
2023-11-29 15:39:51 -0300

    Changed method UploadFileToStaticFiles to prevent path conflicts

--- backend/Services/ImageUploaderService.cs
+++ backend/Services/ImageUploaderService.cs
@@ -8,7 +8,9 @@ public class ImageUploaderService
     if (!Directory.Exists(path)) Directory.CreateDirectory(path);
     FileInfo fileInfo = new FileInfo(file.FileName);
     string fileName = file.FileName;
-    string fileNameWithPath = Path.Combine(path, fileName);
+    string fileNameSplit = fileName.Split(fileInfo.Extension)[0];
+    long currentDate = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds();
+    string fileNameWithPath = Path.Combine(path, fileNameSplit + currentDate + fileInfo.Extension);
     using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
     {
       await file.CopyToAsync(stream);
